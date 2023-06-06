ae5a6f536790e89910e6a29d756221bcd70953be
Santa-Pawn <davis.umar1@gmail.com>
2023-06-06 12:42:04 -0500

    add contract model

--- landlord/models.py
+++ landlord/models.py
@@ -9,17 +9,36 @@ class Property(models.Model):
     landlord = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Room(models.Model):
-    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    property_ = models.ForeignKey(Property, on_delete=models.CASCADE)
     tenant = models.ForeignKey(User)
     status = models.CharField(max_length=200)
     charge = models.FloatField()
 
+
+options = [
+    ('ten', 'tenant'),
+    ('rm', 'room'),
+    ('prop', 'property'),
+    ('oth', 'others'), 
+]
 class Complaint(models.Model):
-    kind = models.CharField(max_length=200)
+    kind = models.CharField(max_length=200, choices=options)
     summary = models.CharField(max_length=200)
+    tenant = models.ForeignKey(User)
+    room = models.ForeignKey(Room)
+    property_ = models.ForeignKey(Property)
+    others = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     reporter = models.ForeignKeyField(User,)
     status = models.CharField(max_length=200)
+
+
+class Contract(models.Model):
+    landlord = models.ForeignKey(User)
+    tenant = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room)
+    contract_doc = models.FileField(upload_to='')
+    contract_accepted = models.BooleanField(null=True, verbose_name="I Agree.")
 
 
     
