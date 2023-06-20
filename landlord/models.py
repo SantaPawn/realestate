ae07deeac5ed6ad2ba25ffbea48b3c3d719b737d
Abubakr-Sadik Nii Nai Davis <dwa2pac@gmail.com>
2023-06-20 20:25:56 +0000

    fix: styling issues in model file

--- landlord/models.py
+++ landlord/models.py
@@ -14,24 +14,23 @@ class Room(models.Model):
     status = models.CharField(max_length=200)
     charge = models.FloatField()
 
-
 options = [
     ('ten', 'tenant'),
     ('rm', 'room'),
     ('prop', 'property'),
     ('oth', 'others'), 
 ]
+
 class Complaint(models.Model):
     kind = models.CharField(max_length=200, choices=options)
     summary = models.CharField(max_length=200)
     tenant = models.ForeignKey(User)
     room = models.ForeignKey(Room)
-    property = models.ForeignKey(Property)
+    property_ = models.ForeignKey(Property)
     others = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     reporter = models.ForeignKeyField(User,)
     status = models.CharField(max_length=200)
-
 
 class Contract(models.Model):
     landlord = models.ForeignKey(User)
@@ -39,6 +38,3 @@ class Contract(models.Model):
     room = models.ForeignKey(Room)
     contract_document = models.FileField(upload_to='')
     contract_accepted = models.BooleanField(default= false)
-
-
-    
